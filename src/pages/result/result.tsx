7ff054107ce00b80b4ed8e214468df098697d36e
thduy2003 <duytrieudong@gmail.com>
2024-11-03 00:59:30 +0700

    add back to homepage button

--- src/pages/result/result.tsx
+++ src/pages/result/result.tsx
@@ -1,8 +1,12 @@
+import { Button } from "antd";
 import useLocalStorage from "../../hooks/useLocalStorageDecode";
+import { useNavigate } from "react-router-dom";
 
 const Result = () => {
   const { storedData: studentInfo } = useLocalStorage("studentInfo");
   const { storedData: questionsInfo } = useLocalStorage("questions");
+  const navigate = useNavigate();
+
   return (
     <div className='flex flex-col w-full max-w-[1300px] justify-center mx-auto p-5'>
       <div className='text-2xl font-bold'>Congratulations</div>
@@ -10,6 +14,15 @@ const Result = () => {
       <div className='flex flex-col'>
         Bạn đã hoàn thành {studentInfo.result}/{questionsInfo.length} câu trả lời đúng
       </div>
+      <Button
+        onClick={() => {
+          navigate("/");
+        }}
+        type='primary'
+        className='mt-4 ml-4'
+      >
+        QUAY LẠI TRANG CHỦ
+      </Button>
     </div>
   );
 };
